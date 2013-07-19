2932da0766a767dfc29993b3cc9624d4ce3665ce
Kamil Marszałkowski <k.marszalkowski@gmail.com>
2013-07-19 15:54:21 +0200

    minor change

--- sub-cli.sh
+++ sub-cli.sh
@@ -9,18 +9,20 @@ if [ -z "$1" ]; then
 	exit 0;
 fi;
 
+
 file=`basename "$1"`
 dir=`dirname "$1"`
 len=${#file}
 sub="${file:0:$len-3}txt"
 subSrt="${file:0:$len-3}srt"
 mplayerDump="dumpsub.sub"
+execName=`basename "$0"` 
 
 cd $dir
 qnapi -c -q "$file"
 
 if [ -f "$sub" ]; then
-	mplayer -quiet -vo null -nosound -frames 0 -subcp cp1250 -dumpmicrodvdsub -sub "$sub" "$file" 2>/dev/null > /tmp/sub.log
+	mplayer -quiet -vo null -nosound -frames 0 -subcp cp1250 -dumpmicrodvdsub -sub "$sub" "$file" 2>/dev/null > /tmp/$execName.log
 
 	if [ -f $mplayerDump ]; then
 		rm "$sub"
