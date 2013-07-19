a3a967ce63e4739b898c9c94590d47f7cbd74fbd
Kamil Marszałkowski <k.marszalkowski@gmail.com>
2013-07-19 15:46:11 +0200

    Subtitle download script - cli version

---
+++ sub-cli.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#
+# Download and match movie subtitles from napisprojekt.pl. (cli)
+#
+
+if [ -z "$1" ]; then
+	echo "Brak parametrów"
+	exit 0;
+fi;
+
+file=`basename "$1"`
+dir=`dirname "$1"`
+len=${#file}
+sub="${file:0:$len-3}txt"
+subSrt="${file:0:$len-3}srt"
+mplayerDump="dumpsub.sub"
+
+cd $dir
+qnapi -c -q "$file"
+
+if [ -f "$sub" ]; then
+	mplayer -quiet -vo null -nosound -frames 0 -subcp cp1250 -dumpmicrodvdsub -sub "$sub" "$file" 2>/dev/null > /tmp/sub.log
+
+	if [ -f $mplayerDump ]; then
+		rm "$sub"
+		mv $mplayerDump "$subSrt"
+		echo "Pobrano i dopasowano napisy."
+		echo "Plik: $subSrt"
+	else 
+		echo "Błąd konwersji mplayer"
+	fi
+else
+	echo "Nie znaleziono napisów dla $file"
+fi
